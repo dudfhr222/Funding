6c5c17857fadd3acb831d15fc5aaa00849efb0c9
startyuphoney <96329521+startyuphoney@users.noreply.github.com>
2022-11-14 21:31:27 +0900

    테스트 오류난거 없앰
    테스트 오류난거 없앰2

--- Funding/src/test/java/com/funding/FundingApplicationTests.java
+++ Funding/src/test/java/com/funding/FundingApplicationTests.java
@@ -1,18 +1,6 @@
 package com.funding;
 
-import java.time.LocalDateTime;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.funding.FundArtist.FundArtist;
-import com.funding.FundArtist.FundArtistRepository;
-import com.funding.FundUser.FundUserRepository;
-
-import com.funding.fundBoard.FundBoard;
-import com.funding.fundBoard.FundBoardRepository;
 
 @SpringBootTest
 class FundingApplicationTests {
